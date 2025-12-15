b7cd54cedde13ce95e9785dc3fabfc01dc5c3c77
vanaja1983 <58247463+vanaja1983@users.noreply.github.com>
2025-12-15 14:21:14 +0530

    Update calculate factorial using a function.py

--- calculate factorial using a function.py
+++ calculate factorial using a function.py
@@ -1,16 +1,13 @@
 def fact(num):
     if num==1:
        return 1
-    elif num<=0:
-       print("Invalid entry.Enter a no greater than 0")
-       return None
     else:
         factorial=num*fact(num-1)
         return factorial
 
 x=int(input("Enter a no: "))
-fact(x)
-print(f" The factorial of {x} is {fact(x)}")
-
-
-
+if x>=1:
+    fact(x)
+    print(f" The factorial of {x} is {fact(x)}")
+else:
+    print("Invalid entry.Enter a no greater than 0")
